9e1d84f079a7949c75bdd0f0ffff87ccef67c270
Elise Phan <aliceconnic@gmail.com>
2019-01-13 00:08:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -94,8 +94,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  Movie.delete
+  movie = Movie.find_by(title)
+  movie.delete
 end
 
 def can_destroy_all_items_at_once
